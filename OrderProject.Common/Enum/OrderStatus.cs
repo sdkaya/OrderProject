bd689356b8981ede1a5037f7f6d9f85dee06ef66
SEDA KAYA <sedakaya8876@gmail.com>
2022-06-11 13:46:15 +0300

    migration applied

---
+++ OrderProject.Common/Enum/OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProject.Common.Enum
+{
+    public enum OrderStatus
+    {
+        Created = 10,
+        InProgress = 20,
+        Failed = 30,
+        Completed = 40,
+        Canceled = 50,
+        Returned = 60
+    }
+}
---
+++ OrderProject.DataAccess/OrderDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderProject.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderProject.DataAccess
+{
+    public class OrderDbContext : DbContext
+    {
+        public OrderDbContext(DbContextOptions<OrderDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
